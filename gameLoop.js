9fe2decaeadcc63c9ab1a19af15bc0fa856272df
GamerPerson57 <amlorden21@gmail.com>
2026-04-08 20:26:27 -0400

    1.1

--- gameLoop.js
+++ gameLoop.js
@@ -12,24 +12,24 @@ timer = setInterval(animate, interval);
 function animate() 
 {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    ball.move();
-    if (ball.x + ball.radius > canvas.width) {
-        ball.x = canvas.width - ball.radius;
-        ball.vx *= -1;
-    }
-    if (ball.x - ball.radius < 0) {
-        ball.x = ball.radius;
-        ball.vx *= -1;
-    }
+    // ball.move();
+    // if (ball.x + ball.radius > canvas.width) {
+    //     ball.x = canvas.width - ball.radius;
+    //     ball.vx *= -1;
+    // }
+    // if (ball.x - ball.radius < 0) {
+    //     ball.x = ball.radius;
+    //     ball.vx *= -1;
+    // }
 
-    if (ball.y + ball.radius > canvas.height) {
-        ball.y = canvas.height - ball.radius;
-        ball.vy *= -1;
-    }
-    if (ball.y - ball.radius < 0) {
-        ball.y = ball.radius;
-        ball.vy *= -1;
-    }
+    // if (ball.y + ball.radius > canvas.height) {
+    //     ball.y = canvas.height - ball.radius;
+    //     ball.vy *= -1;
+    // }
+    // if (ball.y - ball.radius < 0) {
+    //     ball.y = ball.radius;
+    //     ball.vy *= -1;
+    // }
 
     ball.draw();
 }
